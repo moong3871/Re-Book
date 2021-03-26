f77e35c7b24d4880aa2a04e90d419bea92c71bad
kim mun jeong <shzmfkdl@naver.com>
2021-03-26 17:36:52 +0900

    임시 push

--- frontend/src/components/detail/Map.vue
+++ frontend/src/components/detail/Map.vue
@@ -32,10 +32,16 @@ export default {
       };
 
       var map = new kakao.maps.Map(container, options); // 지도 생성
+      // var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png', // 마커 이미지 주소
+      // var imageSize = new kakao.maps.Size(64, 69), // 마커 이미지 크기
+      // var imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지 옵션, 마커 좌표와 일치시킬 이미지 안에서의 좌표 설정
+
+      // var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption), // 마커 이미지 생성
       var markerPosition = new kakao.maps.LatLng(36.3457153, 127.3021023); // 마커 표시 위치
       var marker = new kakao.maps.Marker({
         // 마커 생성
         position: markerPosition,
+        // image: markerImage // 마커이미지 설정
       });
       marker.setMap(map); // 마커가 지도 위에 표시
       map.setMapTypeId(kakao.maps.MapTypeId.STANDARD); // 이거 안하면 is defined but not used
