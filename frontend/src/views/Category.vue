e5bda1d7985d40aff3fb89dbfceeb8594cf03580
psjoon33 <tjdwnsdl0303@gmail.com>
2021-03-31 01:46:52 +0900

    카테고리작업중

--- frontend/src/views/Category.vue
+++ frontend/src/views/Category.vue
@@ -1,9 +1,38 @@
 <template>
-  <div>카테고리 페이지</div>
+  <div>
+    <div class="category-container">
+      <div class="big-category-container">
+        <!-- <div class="big-category">한글도서</div> -->
+        <!-- <div class="big-category">외국어도서</div> -->
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+.category-container {
+  width: 100vw;
+}
+.big-category-container {
+  width: 40vw;
+  margin-left: 30vw;
+  height: 70px;
+  background-color: gray;
+}
+.big-category-container:hover {
+  cursor: pointer;
+}
+.big-category {
+  background-color: green;
+  /* transition-property: background-color; */
+  /* transition-duration: 1s; */
+}
+.big-category:hover {
+  width: 200px;
+  cursor: pointer;
+}
+</style>
