639400f2439ac5c9fba1e234655c33d348e320cd
박성준 <tjdwns20303@naver.com>
2021-04-06 21:14:46 +0000

    로그인/회원가입 페이지 디자인

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,69 +1,104 @@
 <template>
   <v-container fill-height fluid class="login-bg">
-    <v-row align="center" justify="center">
+    <v-row align="center" justify="center" class="login-container">
+      <div class="login-header">
+        <img
+          src="@/assets/images/REBOOK-removebg.png"
+          class="login-logo"
+          @click="$router.push('/')"
+        />
+        <v-btn
+          depressed
+          class="login-header-button"
+          height="50"
+          @click="$router.push('/signup')"
+          color="blue-grey"
+        >
+          SIGNUP
+        </v-btn>
+      </div>
       <v-col align="center">
-        <h1 class="title">로그인</h1>
-        <form @submit.prevent="submit">
-          <div
-            class="form-group"
-            :class="{ 'form-group--error': $v.email.$error }"
-          >
-            <v-text-field
-              type="email"
-              v-model.trim="$v.email.$model"
-              solo
-              placeholder="email을 입력해주세요."
-            ></v-text-field>
-          </div>
-          <div class="error" v-if="!$v.email.required">
-            email을 입력해주세요.
-          </div>
-          <div class="error" v-if="!$v.email.email">
-            email을 올바르게 입력해주세요.
-          </div>
-          <div
-            class="form-group"
-            :class="{ 'form-group--error': $v.password.$error }"
-          >
-            <v-text-field
-              type="password"
-              v-model.trim="$v.password.$model"
-              solo
-              placeholder="비밀번호를 입력해주세요."
-            ></v-text-field>
-          </div>
-          <div class="error" v-if="!$v.password.required">
-            비밀번호를 입력해주세요.
-          </div>
-          <div class="error" v-if="!$v.password.minLength">
-            비밀번호는 적어도
-            {{ $v.password.$params.minLength.min }}글자 이상이어야합니다.
-          </div>
-          <v-btn
-            width="400"
-            height="48"
-            color="#FFFFFF"
-            type="submit"
-            :disabled="submitStatus === 'PENDING'"
-            class="login-btn"
-            >로그인</v-btn
-          >
-          <v-btn
-            width="400"
-            height="48"
-            color="#FFFFFF"
-            class="signup-btn"
-            @click="goSignup"
-            >회원가입</v-btn
-          >
-          <p class="typo__p" v-if="submitStatus === 'OK'">
-            Thanks for your submission!
-          </p>
-          <p class="typo__p" v-if="submitStatus === 'ERROR'">
-            모든 항목을 입력해주세요.
-          </p>
-          <p class="typo__p" v-if="submitStatus === 'PENDING'">Sending...</p>
-        </form>
+        <div class="login-box">
+          <h1 class="title" style="font-family: 'Do Hyeon', sans-serif">
+            로그인
+          </h1>
+          <form @submit.prevent="submit">
+            <div
+              class="form-group"
+              :class="{ 'form-group--error': $v.email.$error }"
+            >
+              <v-text-field
+                type="email"
+                v-model.trim="$v.email.$model"
+                solo
+                placeholder="email을 입력해주세요."
+              ></v-text-field>
+            </div>
+            <div class="error" v-if="!$v.email.required">
+              email을 입력해주세요.
+            </div>
+            <div class="error" v-if="!$v.email.email">
+              email을 올바르게 입력해주세요.
+            </div>
+            <div
+              class="form-group"
+              style="margin-bottom: 5px"
+              :class="{ 'form-group--error': $v.password.$error }"
+            >
+              <v-text-field
+                type="password"
+                v-model.trim="$v.password.$model"
+                solo
+                placeholder="비밀번호를 입력해주세요."
+              ></v-text-field>
+            </div>
+            <div class="error" v-if="!$v.password.required">
+              비밀번호를 입력해주세요.
+            </div>
+            <div class="error" v-if="!$v.password.minLength">
+              비밀번호는 적어도
+              {{ $v.password.$params.minLength.min }}글자 이상이어야합니다.
+            </div>
+            <v-btn
+              width="400"
+              height="48"
+              color="#FFFFFF"
+              type="submit"
+              :disabled="submitStatus === 'PENDING'"
+              class="login-btn"
+              style="font-size: 20px"
+              >로그인</v-btn
+            >
+            <v-btn
+              width="400"
+              height="48"
+              color="#FFFFFF"
+              class="signup-btn"
+              @click="goSignup"
+              style="font-size: 20px"
+              >회원가입</v-btn
+            >
+            <p class="typo__p" v-if="submitStatus === 'OK'">
+              Thanks for your submission!
+            </p>
+            <p class="typo__p" v-if="submitStatus === 'ERROR'">
+              모든 항목을 입력해주세요.
+            </p>
+            <p class="typo__p" v-if="submitStatus === 'PENDING'">Sending...</p>
+            <v-btn width="400" height="48">
+              <!-- <GoogleLogin :params="params" b :onSzuccess="onSuccess"> -->
+              <GoogleLogin>
+                <img
+                  alt="googleLogin"
+                  src="https://web-staging.brandi.co.kr/static/3.50.7/images/google-logo.png"
+                />
+                <span class="google-login-text" style="font-size: 17px"
+                  >Google 계정으로 계속하기</span
+                >
+              </GoogleLogin>
+            </v-btn>
+          </form>
+        </div>
       </v-col>
     </v-row>
   </v-container>
@@ -142,26 +177,87 @@ export default {
 </script>
 
 <style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Do+Hyeon&display=swap");
 .login-bg {
-  background-color: #d9d9d9;
-  opacity: 0.8;
-  max-width: 100%;
-  height: 900px;
+  /* background-color: #d9d9d9; */
+  background-image: url("https://miro.medium.com/max/3840/1*Ke9U-3pgDjro0CfeEU5tEw@2x.jpeg");
+  opacity: 1;
+  background-repeat: no-repeat;
+  background-size: cover;
+  /* width: 100vw; */
+  min-height: 100%;
+  max-height: 100%;
+  min-width: 100%;
+  /* height: 100vh; */
   margin: 0;
+  font-family: "Do Hyeon", sans-serif;
+}
+.login-header {
+  /* position: fixed; */
+  /* top: 0; */
+  min-width: 100%;
+  width: 100%;
+  height: 75px;
+  background-color: rgb(0, 0, 0, 0.5);
+  /* z-index: 3000; */
+}
+.login-logo {
+  height: 75px;
+}
+.login-logo:hover {
+  cursor: pointer;
+  /* background-color: black; */
+}
+.login-header-button {
+  /* height: 50px; */
+  max-height: 50px;
+  width: 90px !important;
+  float: right;
+  margin-right: 20px;
+  background-color: black !important;
+  color: white;
+}
+.login-container {
+  max-width: 100vw;
+  width: 100vw;
+  height: 100%;
+  min-height: 1100px;
+  display: flex;
+  background-color: rgb(0, 0, 0, 0.4);
+}
+.login-box {
+  margin-top: -80px;
+  /* background-color: rgb(61, 44, 8, 0.6); */
+  background-color: rgb(0, 0, 0, 0.85);
+  width: 35%;
+  height: 80%;
+  padding-top: 4%;
+  padding-bottom: 4%;
+  max-height: 800px;
+  border-radius: 30px;
 }
 
 .title {
-  margin: 50px;
+  /* margin-top: 20%; */
+  color: white;
+  font-size: 2.5rem;
+  margin-bottom: 5vh;
 }
 
 .v-input {
-  width: 400px;
-  margin: 1rem;
+  width: 65%;
+  height: 15%;
+  max-height: 65%;
+  /* margin: 1%; */
 }
 
 .v-btn {
-  display: block;
+  width: 65%;
+  /* display: block; */
   margin: 1rem;
+  max-width: 65%;
+  min-width: 65%;
+  min-height: 20%;
 }
 
 .form-group__message,
