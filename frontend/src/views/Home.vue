f8c2a0b740edef82a437685e68ad7604dbdd60ba
박성준 <tjdwns20303@naver.com>
2021-04-08 20:29:26 +0000

    home 디자인 거의 완성

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,17 +2,26 @@
   <div class="home">
     <!-- <ImageSlider /> -->
     <Carousel />
+    <Genre3 />
+    <Genre2 />
+    <Genre1 />
   </div>
 </template>
 
 <script>
 // import ImageSlider from "@/components/home/ImageSlider";
 import Carousel from "@/components/home/Carousel";
+import Genre1 from "@/components/home/Genre1";
+import Genre2 from "@/components/home/Genre2";
+import Genre3 from "@/components/home/Genre3";
 export default {
   name: "Home",
   components: {
     // ImageSlider,
     Carousel,
+    Genre1,
+    Genre2,
+    Genre3,
   },
 };
 </script>
